59d5120f78d00c26be50ff2e433211b37c0e16c0
LeeDongMin <admin@313.co.kr>
2021-12-29 15:23:34 +0900

    [SmartCommit] JSTFFW-3 #comment 스크립트 리팩토링 #close #time 1h +review SR @dongmin.lee
    [ Java Service Tree Framework(JSTF)::Frontend-WEB ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/pdServiceList.js
+++ arms/js/pdServiceList.js
@@ -1,7 +1,21 @@
+$(function() {
+    jstreeDataTableReload();
+
+    $('.dataTables_length').find('select:eq(0)').addClass("darkBack");
+    $('.dataTables_length').find('select:eq(0)').css('min-height','30px');
+    //min-height: 30px;
+});
+
 $(function () {
     setSideMenu();
 });
 
+// 제품 상세 보기 클릭 이벤트
+$( "#selectView" ).click(function() {
+    location.href="pdServiceListUpdate.html";
+});
+
+// --- 사이드 메뉴 설정 --- //
 function setSideMenu() {
     setTimeout(function(){
         $('#sidebar_menu_product').attr("class","accordion-toggle active");
@@ -16,4 +30,46 @@ function setSideMenu() {
         $('#sidebar_menu_product_list').css({'color':'lightblue'});
         $('#sidebar_menu_product_list').css({'font-weight':'900'});
     },1000);
+}
+
+// --- 데이터 테이블 설정 --- //
+function jstreeDataTableReload() {
+
+    console.log("href: "+$(location).attr('href'));
+    console.log("protocol: "+$(location).attr('protocol'));
+    console.log("host: "+$(location).attr('host'));
+    console.log("pathname: "+$(location).attr('pathname'));
+    console.log("search: "+$(location).attr('search'));
+    console.log("hostname: "+$(location).attr('hostname'));
+    console.log("port: "+$(location).attr('port'));
+
+    var isDevelopingToRoute = "/auth-anon";
+
+    var tempDataTable = $('#jstreeTable').DataTable({
+        "ajax": {
+            "url": isDevelopingToRoute + "/com/ext/jstree/springHibernate/core/getMonitor.do",
+            "dataSrc": ""
+        },
+        "destroy": true,
+        "processing": true,
+        "responsive": true,
+        "select": true,
+        "columns": [
+            { "data": "c_id" },
+            { "data": "c_parentid" },
+            { "data": "c_position" },
+            { "data": "c_left" },
+            { "data": "c_right" },
+            { "data": "c_level" },
+            { "data": "c_title" },
+            { "data": "c_type" }
+        ]
+    });
+
+    $('#jstreeTable tbody').on('click', 'tr', function () {
+        var data = tempDataTable.row( this ).data();
+        console.log(data);
+        //alert( 'You clicked on '+ data.c_title +'\'s row' );
+    } );
+
 }
