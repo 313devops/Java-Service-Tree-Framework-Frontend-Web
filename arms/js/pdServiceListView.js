ae57eda9763769eb42ffc9db8372f1b84ceb392d
LeeDongMin <admin@313.co.kr>
2021-12-30 19:21:46 +0900

    [SmartCommit] JSTFFW-3 #comment 제품(서비스) 상세보기 누락된 스크립트 추가 #close #time 1h +review SR @dongmin.lee
    [ Java Service Tree Framework(JSTF)::Frontend-WEB ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/pdServiceListView.js
+++ arms/js/pdServiceListView.js
@@ -1,4 +1,12 @@
 $(function () {
+    // --- 에디터 설정 --- //
+    CKEDITOR.replace( 'editor' );
+    CKEDITOR.config.readOnly = true;
+    CKEDITOR.on('instanceReady', function(ev) {
+        var height = $( document ).height() -450;
+        $('.cke_contents').css('height',height+'px');
+    });
+
     setSideMenu();
 });
 
